c8e24c2a973cf1843e076706f83d832fbb095626
Md. Rony <ronimdrahim467@gmail.com>
2025-11-12 19:28:03 +0600

    prost transaction done and overview section done

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4kmqo4g.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4kmqo4g.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,56 +22,93 @@ async function run() {
   try {
     await client.connect();
 
-    const db=client.db('FinEase-db')
-    const transaction=db.collection('transactions')
+    const db = client.db("FinEase-db");
+    const transaction = db.collection("transactions");
 
+    //Transactions get kortesi
+    app.get("/transactions", async (req, res) => {
+      const result = await transaction.find().toArray();
+      res.send(result);
+    });
 
-    //Transactions get kortesi 
-    app.get('/transactions',async(req,res)=>{
-        const result=await transaction.find().toArray()
-        res.send(result)
-    })
-
-    app.get('/transactions/:id',async(req,res)=>{
-      const {id}=req.params;
-      const objectId=new ObjectId(id);
-      const result= await transaction.findOne({_id:objectId})
+    app.get("/transactions/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const result = await transaction.findOne({ _id: objectId });
 
       res.send({
-        result
-      })
-    })
+        result,
+      });
+    });
 
-    app.put('/transactions/:id', async(req,res)=>{
-      const {id}=req.params;
-      const data=req.body;
-      console.log(data)
-      const objectId=new ObjectId(id)
-      const filter={_id: objectId}
-      const update={
-        $set: data
-      }
-      
-      const result= await transaction.updateOne(filter,update);
+    app.put("/transactions/:id", async (req, res) => {
+      const { id } = req.params;
+      const data = req.body;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+      const update = {
+        $set: data,
+      };
 
+      const result = await transaction.updateOne(filter, update);
 
       res.send({
         success: true,
-        result
-      })
-    })
+        result,
+      });
+    });
 
-    //Post A Tranction
-    app.post('/transactions', async(req,res)=>{
-      const data=req.body;
-      const result=await transaction.insertOne(data);
+    app.delete("/transactions/:id", async (req, res) => {
+      const { id } = req.params;
+      const objectId = new ObjectId(id);
+      const filter = { _id: objectId };
+
+      const result = await transaction.deleteOne(filter);
 
       res.send({
-        success: true
-      })
-    })
+        success: true,
+        result,
+      });
+    });
 
-    
+    //Post A Tranction
+   app.post("/transactions", async (req, res) => {
+  const data = req.body;
+
+  if (data.date) {
+    data.date = new Date(data.date + "T00:00:00Z"); // UTC midnight
+  }
+
+  const result = await transaction.insertOne(data);
+  res.send({ success: true, result });
+});
+
+
+   app.get("/month-transactions", async (req, res) => {
+  const now = new Date();
+
+  // Start of current month UTC
+  const startOfMonth = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), 1, 0, 0, 0));
+
+  // End of current month UTC (23:59:59 last day)
+  const endOfMonth = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, 0, 23, 59, 59));
+
+  try {
+    const transactions = await transaction
+      .find({
+        date: { $gte: startOfMonth, $lte: endOfMonth },
+      })
+      .sort({ date: -1 })
+      .toArray();
+
+    console.log("Start:", startOfMonth, "End:", endOfMonth, "Found:", transactions.length);
+
+    res.json(transactions);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
 
 
 
@@ -80,7 +116,6 @@ async function run() {
     app.listen(port, () => {
       console.log(`app listen port ${port}`);
     });
-
 
     await client.db("admin").command({ ping: 1 });
     console.log(
